93bc0edb81396f095ea0ebfa8abd88d88f7676c7
Omnia <omniaeid266@gmail.com>
2023-11-23 16:57:16 -0600

    first commit

---
+++ JenkinsDemo/src/main/java/com/example/demo/controller/JenkinsController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Jenkins;
+import com.example.demo.repo.JenkinsRepo;
+
+
+
+@RestController
+public class JenkinsController {
+	
+	@Autowired
+	JenkinsRepo jenkinsRepo;
+	
+     @PostMapping("/add")
+	public void  hello(@RequestBody Jenkins jenkins) {
+		
+	jenkinsRepo.save(jenkins);
+		
+	}
+
+     
+@GetMapping("/hi")
+ 	public String  hello() {
+ 		
+ 	return"hellllllo";
+ 		
+ 	}
+}
